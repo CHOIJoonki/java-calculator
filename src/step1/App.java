121a44e985cbed300fff54e0e39c41c25aa411b4
CHOIJoonki <scarmic@naver.com>
2026-01-09 11:34:01 +0900

    양의 정수만 입력가능

--- src/step1/App.java
+++ src/step1/App.java
@@ -10,12 +10,20 @@ public class App {
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
+            if (num1 < 0) {
+                System.out.println("잘못된 숫자입니다. 양의 정수만 입력해주세요");
+                continue;
+            }
 
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
             System.out.print("두 번째 숫자를 입력하세요: ");
             int num2 = sc.nextInt();
+            if (num2 < 0) {
+                System.out.println("잘못된 숫자입니다. 양의 정수만 입력해주세요");
+                continue;
+            }
 
             int result = 0;
 
