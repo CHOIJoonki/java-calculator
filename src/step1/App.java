15d472d11500bfc646ad3ecf3b4d0e3a872d8156
CHOIJoonki <scarmic@naver.com>
2026-01-09 11:07:38 +0900

    사칙연산 기본 구현 확인

--- src/step1/App.java
+++ src/step1/App.java
@@ -20,6 +20,18 @@ public class App {
 
             int result = 0;
 
+            if (operator == '+') {
+                result = num1 + num2;
+            } else if (operator == '-') {
+                result = num1 - num2;
+            } else if (operator == '*') {
+                result = num1 * num2;
+            } else if (operator == '/') {
+                result = num1 / num2;
+            } else {
+                System.out.println("잘못된 연산자입니다.");
+            }
+
             System.out.println("결과: " + result);
 
 
